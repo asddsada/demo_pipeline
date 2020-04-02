30c62b90cf8ed70c17d9c9508f57d46424c71e68
asddsada <baipo_p@hotmail.com>
2020-04-02 10:30:09 +0700

    Update to fix bug

--- run_demo_pipeline.sh
+++ run_demo_pipeline.sh
@@ -10,8 +10,10 @@ target_spk="target"
 . ./utils/parse_options.sh
 
 if [ $stage -le 0 ]; then
+    if [ -d data/test_roc ]; then rm -rf data/test_demo; fi
     ./inuse_script/data_prep_gowajee.sh audios_demo/ data/test_demo/ || (echo "ERROR 0" > report_outputs/log && exit 1);
     ./utils/fix_data_dir.sh data/test_demo || (echo "ERROR 0" > report_outputs/log && exit 1);
+    ./steps/make_mfcc.sh --write-utt2num-frames true --mfcc-config conf/mfcc.conf --nj 1 --cmd "$train_cmd" data/test_demo exp/make_mfcc mfcc || (echo "ERROR 0" > report_outputs/log && exit 1);
 fi
 
 if [ $stage -le 1 ]; then
@@ -19,7 +21,7 @@ if [ $stage -le 1 ]; then
 fi
 
 if [ $stage -le 2 ]; then
-    ./inuse_script/eval_with_unlabel.sh --test-set test_combined --unlabel train_gowajee --nj 32 --make-unlabel 0 --centering-unlabel 0 --train-plda-unlabel 0 --eval-unlabel 1 --stage 0  || (echo "ERROR 2" > report_outputs/log  && exit 1);
+    ./inuse_script/eval_with_unlabel.sh --test-set test_combined --unlabel train_gowajee --nj 8 --make-unlabel 0 --centering-unlabel 0 --train-plda-unlabel 0 --eval-unlabel 1 --stage 0  || (echo "ERROR 2" > report_outputs/log  && exit 1);
 fi
 
 if [ $stage -le 3 ]; then
